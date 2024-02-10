dc19e5947ed3bb8dca3acbc83fc526919462f820
Amar Bajraktarevic <157768628+AmarBajraktarevic@users.noreply.github.com>
2024-02-10 18:29:01 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import cover from './images/cover.png';
 import wokeup from './song/wokeup.mp3';
 import bg from './videos/bg.mp4';
 import git from './images/git2.png';
+
+
 function App() {
   const [viewCount, setViewCount] = useState(2872);
   const [currentTime, setCurrentTime] = useState(0);
@@ -20,6 +22,8 @@ function App() {
   const [copyStatus, setCopyStatus] = useState('');
   const [cssLabel, setCssLabel] = useState('Copy BTC Address');
   const [cssLabel1, setCssLabel1] = useState('Copy LTC Address');
+  const [entered, setEntered] = useState(false); // State for animation
+
   useEffect(() => {
     fetch('/increment-view')
       .then(response => response.json())
@@ -83,8 +87,6 @@ function App() {
       .catch(error => console.error('Error copying address to clipboard:', error));
   };
   
-  
-
   function audioPlay() {
     var audio = document.getElementById('audio');
     audio.volume = 0.5;
@@ -105,35 +107,26 @@ function App() {
     setShowOverlay(false);
     setIsOverlayClicked(true);
     audioPlay();
+    setEntered(true); // Trigger the animation
   };
 
   return (
-    
-    
     <div className='app-container'>
-        {}
-    <video autoPlay loop muted className='video-background'>
-      <source src={bg} type='video/mp4' />
-      Your browser does not support the video tag.
-    </video>
-      
-
-      
-      
-      {}
+      <video autoPlay loop muted className='video-background'>
+        <source src={bg} type='video/mp4' />
+        Your browser does not support the video tag.
+      </video>
       {showOverlay && (
         <div className='overlay' onClick={handleOverlayClick}>
           <p1 className='click'>Click Anywhere</p1>
         </div>
       )}
-
-      <div className='main-container'>
+      <div className={`main-container ${entered ? 'entered' : ''}`}>
         <img src={view} className='view' alt="View Icon" />
         <p1 className='num'>{viewCount}</p1>
         <img src={pfp} className='pfp' alt="Profile Picture" />
         <h1 className='name'>shadow</h1>
         <h1 className='bio'>Owner of shdw.site</h1>
-
         <div className='links'>
           <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
             <img src={twitter} className='link1' alt="Twitter" />
@@ -152,20 +145,13 @@ function App() {
           </a>
         </div>
         <div className='div1'></div>
-
         <div className='song'>
           <div className='progress-bar-container'>
             <div className='progress-bar' style={{ width: `${(currentTime / maxTime) * 100}%` }} />
           </div>
-
           <a href='https://open.spotify.com/track/3jM9VzF2d1AMUyTD8sTmPu?si=0276064ed0814fdf&nd=1&dlsi=f95a64e213a049ce' target='_blank' rel='noopener noreferrer'>
-          <img src={cover} className='songcover' alt='' />
+            <img src={cover} className='songcover' alt='' />
           </a>
-
-       
-
-        
-
           <div className='songinfo'>
             <p1 className='songtitle'>Wokeup</p1>
             <p1 className='artist'>by Kankan</p1>
@@ -174,32 +160,23 @@ function App() {
           <div className='time-label'>
             {formatTime(currentTime)} / {formatTime(maxTime)}
           </div>
-
-          {}
           <audio id='audio' src={wokeup} />
         </div>
-
         <div className='div2'></div>
-
         <button
-  className='button2'
-  onClick={() => handleCopyAddress1('ltc1qwu5kth29x3ev63fuex4ln873kdwdxdnvw0fvdq', 'LTC Address')}
-  data-label={cssLabel1}
->
-  LTC
-</button>
-
-<button
-  className='button1'
-  onClick={() => handleCopyAddress('bc1qmdde26zln58kprcz2fxf7980ad9jfnpxa7ev4p', 'BTC Address')}
-  data-label={cssLabel}
->
-  BTC
-</button>
-
-
-
-
+          className='button2'
+          onClick={() => handleCopyAddress1('ltc1qwu5kth29x3ev63fuex4ln873kdwdxdnvw0fvdq', 'LTC Address')}
+          data-label={cssLabel1}
+        >
+          LTC
+        </button>
+        <button
+          className='button1'
+          onClick={() => handleCopyAddress('bc1qmdde26zln58kprcz2fxf7980ad9jfnpxa7ev4p', 'BTC Address')}
+          data-label={cssLabel}
+        >
+          BTC
+        </button>
       </div>
     </div>
   );
